a880f2095dedda6244f31ec9e783eef437098f84
German <gaordonezh@gmail.com>
2022-08-31 16:38:23 -0500

    NEW CHANGES

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,15 +1,62 @@
-import { StyleSheet } from 'react-native';
+import { useEffect, useState } from "react";
+import { Button, StyleSheet } from "react-native";
 
-import EditScreenInfo from '../components/EditScreenInfo';
-import { Text, View } from '../components/Themed';
-import { RootTabScreenProps } from '../types';
+import EditScreenInfo from "../components/EditScreenInfo";
+import { Text, View } from "../components/Themed";
+import { RootTabScreenProps } from "../types";
+import { BarCodeScanner } from "expo-barcode-scanner";
 
-export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'>) {
+export default function TabOneScreen({ navigation }: RootTabScreenProps<"TabOne">) {
+  const [hasPermission, setHasPermission] = useState<boolean | null>(null);
+  const [scanned, setScanned] = useState(false);
+  const [text, setText] = useState("Not yet scanned");
+
+  const askForCameraPermission = () => {
+    (async () => {
+      const { status } = await BarCodeScanner.requestPermissionsAsync();
+      setHasPermission(status === "granted");
+    })();
+  };
+
+  useEffect(() => {
+    askForCameraPermission();
+  }, []);
+
+  const handleBarCode = ({ type, data }: any) => {
+    setScanned(true);
+    setText(data);
+    console.log(type, data);
+  };
+
+  if (hasPermission === null) {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.title}>REQUESTING FOR CAMERA PERMISSION</Text>
+      </View>
+    );
+  }
+
+  if (hasPermission === false) {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.title}>CAMERA PERMISSION WAS DENIED</Text>
+        <Button title="Allow Camera" onPress={askForCameraPermission} />
+      </View>
+    );
+  }
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/TabOneScreen.tsx" />
+      <View style={styles.barcodebox}>
+        <BarCodeScanner
+          onBarCodeScanned={scanned ? undefined : handleBarCode}
+          style={{ height: 400, width: 400 }}
+        />
+
+        <Text style={styles.maintext}>{text}</Text>
+
+        {scanned && <Button title="Scan again" onPress={() => setScanned(false)} color="tomato" />}
+      </View>
     </View>
   );
 }
@@ -17,16 +64,23 @@ export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
   title: {
     fontSize: 20,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
+  barcodebox: {
+    alignItems: "center",
+    justifyContent: "center",
+    height: "auto",
+    width: 227,
+    overflow: "hidden",
+    borderRadius: 30,
+  },
+  maintext: {
+    fontSize: 16,
+    margin: 20,
   },
 });
